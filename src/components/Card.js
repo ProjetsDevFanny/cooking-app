7e3c61e4fdf4b715345f4562130bff95e0aecc38
Fanny Simon <contact.fanny.simon@gmail.com>
2025-05-20 15:34:09 +0200

    Second fix ESLint warning in Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ meal }) => {
     <li className="card">
       <h2>{meal.strMeal}</h2>
       <p>Origin : {meal.strArea}</p>
-      <img src={meal.strMealThumb} alt={`Meal picture of ${meal.strMeal}`} />
+      <img src={meal.strMealThumb} alt={`Meal of ${meal.strMeal}`} />
       <p id="instructions">{meal.strInstructions}</p>
     </li>
   );
